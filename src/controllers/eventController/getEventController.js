1c2ecadcac155c6752f207b4e3f6d55f40c9cf4f
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-04-04 11:05:14 -0500

    getEventHandler refactored

--- src/controllers/eventController/getEventController.js
+++ src/controllers/eventController/getEventController.js
@@ -1,11 +1,15 @@
-const { Event } = require("../../db");
+const { Event, Ticket, Sponsor } = require("../../db");
 
 const getEventController = async (id) => {
   try {
-    const event = await Event.findByPk(id);
+    const event = await Event.findByPk(id, {
+      include: [Ticket, Sponsor], 
+    });
+
     if (!event) {
       return { success: false, msg: "Event not found" };
     }
+
     return { success: true, event };
   } catch (error) {
     console.error("Error fetching event:", error);
