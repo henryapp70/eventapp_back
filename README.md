690aff7ec2dbfa19fcf183eff2e52b15f2fbbb89
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-04-08 17:11:20 -0500

    two additional endpoints added

--- README.md
+++ README.md
@@ -12,3 +12,19 @@ endpoints:
         http://localhost:3001/api/v1/registerevent/:id
         http://localhost:3001/api/v1/updateevent/:idUser/:idEvent
         http://localhost:3001/api/v1/deleteevent/:idUser/:idEvent
+
+        to update event status
+        http://localhost:3001/api/v1/updateevent/:idUser/:idEvent/:status
+        http://localhost:3001/api/v1/updateevent/2/1/false
+
+    Sponsors:
+        http://localhost:3001/api/v1/sponsor
+        http://localhost:3001/api/v1/sponsor
+        http://localhost:3001/api/v1/sponsor/:id
+        http://localhost:3001/api/v1/sponsor/:id
+
+        to update sponsor status
+        http://localhost:3001/api/v1/sponsor/:idSponsor/:status
+        http://localhost:3001/api/v1/sponsor/1/false
+
+
--- src/routes/sponsorRouter.js
+++ src/routes/sponsorRouter.js
@@ -4,10 +4,13 @@ const getAllSponsorHandler = require("../handlers/sponsorHandler/getAllSponsorHa
 const registerSponsorHandler = require("../handlers/sponsorHandler/registerSponsorHandler");
 const updateSponsorHandler = require("../handlers/sponsorHandler/updateSponsorHandler");
 const deleteSponsorHandler = require("../handlers/sponsorHandler/deleteSponsorHandler");
+const updateSponsorStatusHandler = require("../handlers/sponsorHandler/updateSponsorStatusHandler");
+
 
 router.route("/sponsor").get(getAllSponsorHandler); 
 router.route("/sponsor").post(registerSponsorHandler); 
+router.route("/sponsor/:id/:status").put(updateSponsorStatusHandler);
 router.route("/sponsor/:id").put(updateSponsorHandler); 
-router.route("/sponsor/:id").delete(deleteSponsorHandler); 
+router.route("/sponsor/:id").delete(deleteSponsorHandler);
 
 module.exports = router;
