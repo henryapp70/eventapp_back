0a563c98f58895ffe369614627f6136d854b190f
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-04-11 17:54:02 -0500

    purchasing details persisted

--- src/controllers/paymentController/createPaymentController.js
+++ src/controllers/paymentController/createPaymentController.js
@@ -1,26 +1,38 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const { Purchase } = require('../../db')
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const { Purchase, Event, Ticket } = require("../../db");
 const createSession = async (req, res) => {
   try {
-    const { eventName, eventPrice } = req.body; // Obtener el nombre y el precio del evento desde el cuerpo de la solicitud
-    // Recibiría id_ticket, id_user, quantity_tickests
+    // const { eventName, eventPrice } = req.body; // Obtener el nombre y el precio del evento desde el cuerpo de la solicitud
+    const { id_ticket, id_user, quantity } = req.body;
     // Crear la sesión de pago en Stripe con los datos del evento
+    const ticket = await Ticket.findOne({ where: { id_ticket } });
+    const event = await Event.findOne({ where: { id_ticket } });
+
     const session = await stripe.checkout.sessions.create({
-      line_items: [{
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: eventName, // Usar el nombre del evento como nombre del producto en Stripe
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: event.name, // Usar el nombre del evento como nombre del producto en Stripe
+            },
+            unit_amount: ticket.price * 100, // Convertir el precio a centavos (Stripe utiliza el valor en centavos)
           },
-          unit_amount: eventPrice * 100, // Convertir el precio a centavos (Stripe utiliza el valor en centavos)
+          quantity: quantity,
         },
-        quantity: 1,
-      }],
-      mode: 'payment',
-      success_url: 'http://localhost:5173/#/succes',
-      cancel_url: 'http://localhost:5173/api/v1/payment/#/purchase',
+      ],
+      mode: "payment",
+      success_url: "http://localhost:5173/#/succes",
+      cancel_url: "http://localhost:5173/api/v1/payment/#/purchase",
     });
-        
+
+    const purchaseDetails = {
+      id_user,
+      quantity,
+      id_ticket,
+    };
+    const purchase = await Purchase.create(purchaseDetails);
+
     // Devolver el ID de la sesión de pago creada
     return session;
   } catch (error) {
