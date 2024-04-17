717b8cdebf70d567965ca80f82ae0ab4e130f6ce
woquintela <walterquintela@gmail.com>
2024-04-17 19:56:52 -0400

    confirmado

--- src/db.js
+++ src/db.js
@@ -7,8 +7,8 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DATABASE_URL } =
   process.env;
 
 const sequelize = new Sequelize(
-  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, // descomentar para probar localmente
-  DATABASE_URL,  // comentar para probar localmente
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, // descomentar para probar localmente
+  //DATABASE_URL,  // comentar para probar localmente
   {
     dialect: "postgres",
     logging: false,
