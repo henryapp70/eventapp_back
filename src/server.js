0a2f34afb9f1e840169a009b9e8ebcfcfa87e5df
Abril31 <brivaldi0416@gmail.com>
2024-04-23 23:11:32 -0400

    restableciendo cors

--- src/server.js
+++ src/server.js
@@ -8,12 +8,6 @@ const server = express();
 server.use(morgan("dev"));
 server.use(express.json());
 
-// Establecer el encabezado Access-Control-Allow-Origin
-server.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
-
 server.use(cors());
 server.use(router);
 
